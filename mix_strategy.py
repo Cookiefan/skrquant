4bc8aaf6fa852bb50de2fa6ca1d049bdcc861a52
cookie  <cookie@hit.edu.cn>
2019-07-06 14:14:13 +0800

    update skr~

--- mix_strategy.py
+++ mix_strategy.py
@@ -5,14 +5,17 @@ import backtrader as bt
 from utils import HS300Data, code300
 from tqdm import tqdm
 from strategy.SkrStrategy import SkrStrategy
+from indicators.MomentumIndicator import Momentum
+
 
 class CrossLineStrategy(SkrStrategy):
-    params = (
-        ('ranker', 0.2),
-        ('global_period', 100),
-        ('short_period', 5),
-        ('long_period', 60),
-        ('atr_period', 20),
+    params = dict(
+        tuning=False,
+        ranker=0.2,
+        global_period=100,
+        short_period=14,
+        long_period=80,
+        atr_period=20
     )
 
     def __init__(self):
@@ -26,11 +29,14 @@ class CrossLineStrategy(SkrStrategy):
             self.inds[s] = {}
             self.inds[s]["short_ma"] = bt.indicators.SimpleMovingAverage(s.close, period=self.p.short_period)
             self.inds[s]["long_ma"] = bt.indicators.SimpleMovingAverage(s.close, period=self.p.long_period)
-            self.inds[s]["cross"] = self.inds[s]["short_ma"] - self.inds[s]["long_ma"]
+            self.inds[s]["cross"] = (self.inds[s]["short_ma"] - self.inds[s]["long_ma"])/self.inds[s]["long_ma"]
+            self.inds[s]["ref"] = self.inds[s]["cross"]
             self.inds[s]["atr20"] = bt.indicators.ATR(s, period=self.p.atr_period)
 
-    def prenext(self):
-        self.next()
+    def start(self):
+        if self.p.tuning:
+            self.tim = tqdm(total=608,desc = 'opt(%f):'%(self.p.ranker))
+
 
     def next(self):
         if self.t % 5 == 0:
@@ -38,21 +44,26 @@ class CrossLineStrategy(SkrStrategy):
         if self.t % 10 == 0:
             self.rebalance_positions()
         self.t += 1
+        if self.p.tuning:
+            self.tim.update(1)
 
     def stop(self):
-        self.log('(MA Period (%d, %d)) Ending Value %.2f' %
-                 (self.p.short_period, self.p.long_period, self.broker.getvalue()))
+        if self.p.tuning:
+            self.tim.close()
+            self.log('({rk}) -> Ending Value {va}'
+                     .format(rk=self.p.ranker,
+                             va=self.broker.getvalue()))
 
     def rebalance_portfolio(self):
         self.rankings = list(filter(lambda s: len(s) > 100, self.stocks))
-        self.rankings.sort(key=lambda s: self.inds[s]["cross"])
+        self.rankings.sort(key=lambda s: self.inds[s]["ref"])
         num_stocks = len(self.rankings)
         for i, s in enumerate(self.rankings[int(num_stocks * self.p.ranker):]):
             if self.getposition(s).size:
                 self.close(s)
 
-        # if self.hs300.close < self.hs300_ma:
-        #     return
+        if self.hs300.close < self.hs300_ma:
+            return
         for i, s in enumerate(self.rankings[:int(num_stocks * self.p.ranker)]):
             cash = self.broker.get_cash()
             value = self.broker.get_value()
@@ -64,8 +75,8 @@ class CrossLineStrategy(SkrStrategy):
 
     def rebalance_positions(self):
         num_stocks = len(self.rankings)
-        # if self.hs300.close < self.hs300_ma:
-        #     return
+        if self.hs300.close < self.hs300_ma:
+            return
         for i, s in enumerate(self.rankings[:int(num_stocks * self.p.ranker)]):
             cash = self.broker.get_cash()
             value = self.broker.get_value()
@@ -73,35 +84,6 @@ class CrossLineStrategy(SkrStrategy):
                 break
             size = value * 0.001 / self.inds[s]["atr20"]
             self.order_target_size(s, size)
-
-
-class OrderObserver(bt.observer.Observer):
-    lines = ('created', 'expired',)
-
-    plotinfo = dict(plot=True, subplot=True, plotlinelabels=True)
-
-    plotlines = dict(
-        created=dict(marker='*', markersize=8.0, color='lime', fillstyle='full'),
-        expired=dict(marker='s', markersize=8.0, color='red', fillstyle='full')
-    )
-
-    def next(self):
-        for order in self._owner._orderspending:
-            if order.data is not self.data:
-                continue
-
-            if not order.isbuy():
-                continue
-
-            # Only interested in "buy" orders, because the sell orders
-            # in the strategy are Market orders and will be immediately
-            # executed
-
-            if order.status in [bt.Order.Accepted, bt.Order.Submitted]:
-                self.lines.created[0] = order.created.price
-
-            elif order.status in [bt.Order.Expired]:
-                self.lines.expired[0] = order.created.price
 
 
 if __name__ == '__main__':
@@ -119,12 +101,13 @@ if __name__ == '__main__':
             cerebro.adddata(bt.feeds.PandasData(dataname=df, plot=False))
 
     cerebro.addstrategy(CrossLineStrategy)
-    # cerebro.optstrategy(CrossLineStrategy, message_order = False, short_period=range(3, 10, 1), long_period = range(10, 100, 10))
-    cerebro.broker.setcash(100000.0)
-    cerebro.addobserver(OrderObserver)
     cerebro.addanalyzer(bt.analyzers.SharpeRatio, riskfreerate=0.0)
     cerebro.addanalyzer(bt.analyzers.Returns)
     cerebro.addanalyzer(bt.analyzers.DrawDown)
+    # cerebro.optstrategy(CrossLineStrategy,
+    #                     tuning=True,
+    #                     order_print=False,
+    #                     ranker = [0.1,0.2,0.3,0.4,0.5])
     results = cerebro.run()
     cerebro.plot(iplot=False)
     print(f"Sharpe: {results[0].analyzers.sharperatio.get_analysis()['sharperatio']:.3f}")
